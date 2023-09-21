94219187916f11381e08fdf3b2fc6d9189a76307
Fabian Pedregosa <pedregosa@google.com>
2023-09-21 09:06:38 +0200

    FIX: use docker instead of dockerhub and update README

--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -2,6 +2,7 @@ FILE=Gemfile.lock
 if [ -f "$FILE" ]; then
     rm $FILE
 fi
+docker build -t "al-folio:latest" . 
 docker run --rm -v "$PWD:/srv/jekyll/" -p "8080:8080" \
                     -it al-folio:latest bundler  \
                     exec jekyll serve --future --watch --port=8080 --host=0.0.0.0 
