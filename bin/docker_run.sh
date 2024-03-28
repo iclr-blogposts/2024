7108ade54afaea4469e2bbdf468aa9ff987dbc56
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 16:55:06 +0000

    Revert "trying to remove 2024-05-07-distill-example.md"
    This reverts commit 319fc5152bd54c58313991eca4edc5b296b1179f. [ci skip]

--- bin/docker_run.sh
+++
@@ -1,8 +0,0 @@
-FILE=Gemfile.lock
-if [ -f "$FILE" ]; then
-    rm $FILE
-fi
-docker build -t "iclr-2024:latest" . && \
-docker run --rm -v "$PWD:/srv/jekyll/" -p "8080:8080" \
-    -it iclr-2024:latest bundler \
-    exec jekyll serve --trace --future --watch --port=8080 --host=0.0.0.0
